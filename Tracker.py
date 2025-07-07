7fa1e0594ff3d99a624d247434983a3dcd2b8a0a
Leprex24 <kacpersaw23@gmail.com>
2025-07-07 20:28:18 +0200

    Add delete expense functionality

--- Tracker.py
+++ Tracker.py
@@ -60,24 +60,42 @@ def list_expenses(args):
         reader = csv.reader(csvfile)
         next(reader)
         all_rows = list(reader)
-        data = []
-        if not all_rows:
-            print("Nie dodano jeszcze zadnych wydatkow")
-            return
+    data = []
+    if not all_rows:
+        print("Nie dodano jeszcze zadnych wydatkow")
+        return
 
-        if category == None:
-            data = all_rows
-        else:
-            data = [row for row in all_rows if row[4] == category]
+    if category == None:
+        data = all_rows
+    else:
+        data = [row for row in all_rows if row[4] == category]
 
-        for row in data:
-            sum += float(row[3])
+    for row in data:
+        sum += float(row[3])
 
-        if data:
-            print(tabulate.tabulate(data, headers=['ID', 'Data', 'Opis', 'Kwota', 'Kategoria'], tablefmt="github"))
-            print(f"Suma: {sum:.2f} zl")
-        else:
-            print("Brak wydatków do wyswietlenia")
+    if data:
+        print(tabulate.tabulate(data, headers=['ID', 'Data', 'Opis', 'Kwota', 'Kategoria'], tablefmt="github"))
+        print(f"Suma: {sum:.2f} zl")
+    else:
+        print("Brak wydatków do wyswietlenia")
+
+def delete_expense(args):
+    ID = args.id
+    with open('wydatki.csv', 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        all_rows = list(reader)
+        ID_list = [int(row[0]) for row in all_rows]
+    if ID not in ID_list:
+        print(f"Nie mozna usunac wydatku o ID: {ID}, poniewaz takowy nie istnieje!")
+    else:
+        all_rows = [row for row in all_rows if int(row[0]) != ID]
+        with open('wydatki.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['ID', 'Data', 'Opis', 'Kwota', 'Kategoria'])
+            writer.writerows(all_rows)
+        print(f"Usunieto wydatek (ID: {ID})")
+
 
 
 def main():
@@ -95,6 +113,9 @@ def main():
     list_parser = subparsers.add_parser('wypisz', help='Wypisz wszystkie wydatki')
     list_parser.add_argument('-k', '--kategoria', choices=['Jedzenie', 'Zakupy', 'Transport', 'Rozrywka' ], help='Kategoria wydatku')
 
+    delete_parser = subparsers.add_parser('usun', help='Usun istniejacy wydatek')
+    delete_parser.add_argument('-i', '--id', type=int, required=True, help='ID wydatku do usuniecia')
+
     args = parser.parse_args()
 
     if args.mode == 'dodaj':
@@ -102,6 +123,9 @@ def main():
 
     if args.mode == 'wypisz':
         list_expenses(args)
+
+    if args.mode == 'usun':
+        delete_expense(args)
 
     if not args.mode:
         parser.print_help()
